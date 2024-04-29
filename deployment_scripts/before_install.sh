1cd932469d6087fbcc5a130d36b5ce56ee2c8e14
sami-2905 <89797985+sami-2905@users.noreply.github.com>
2024-04-30 01:09:48 +0530

    Update before_install.sh

--- deployment_scripts/before_install.sh
+++ deployment_scripts/before_install.sh
@@ -1,14 +1,14 @@
 #sudo !/bin/bash
-FILE=/home/ubuntu/app/index.js
+FILE=/home/ec2-user/app/index.js
 if test -f "$FILE"; then
    echo "$FILE exists"
-   cd /home/ubuntu/
+   cd /home/ec2-user/
    pm2 stop --silent index.js
    pm2 delete index.js
    killall -9 node
 else 
     echo "$FILE does not exist."
 fi
-cd /home/ubuntu/
+cd /home/ec2-user/
 sudo rm -rf app
 sudo mkdir app
